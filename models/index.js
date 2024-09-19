d5e380f14f70d784be1774651fa2b9cd468c7267
StitchDaBih <sarah.hamp93@gmail.com>
2024-09-18 19:25:23 -0500

    updates
    css and functional updates
    
    Co-Authored-By: SelfMadeMilli <171191053+SelfMadeMilli@users.noreply.github.com>
    Co-Authored-By: Anthony Black <anthonyblack1992@gmail.com>
    Co-Authored-By: SalvadorVasquez <170376780+SalvadorVasquez@users.noreply.github.com>

--- models/index.js
+++ models/index.js
@@ -1,21 +1,25 @@
 const User = require('./User');
 const Project = require('./Project');
 const Affirmation = require('./Affirmation');
+const context = {
+  backgroundImage:
+    '/Users/stitchdabih/Documents/GitHub/TimeManagment/assets/images/numerology-bg.jpg',
+};
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Project.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 User.hasMany(Affirmation, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 Affirmation.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Project, Affirmation  };
+module.exports = { User, Project, Affirmation };
